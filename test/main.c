5890ef656a67103fb4553a18b54d210fb65cf45c
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-08-20 06:32:12 -0700

    doc: check .gitignore; use -f flag when adding this

---
+++ test/main.c
@@ -0,0 +1,17 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Compiled!\n");
+	_printf("%s", "Ama\n");
+	_printf("Kofi is a boy.\n");
+	_printf("This is a very long string djfkljfkjf kjfkjfkljflkdj \n");
+	return (0);
+}
