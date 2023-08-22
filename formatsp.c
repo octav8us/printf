0c59c24fd08a05f2dbb8ee507e3efff5682e7e9a
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-08-21 20:14:42 -0700

    Revert "handle nonprintable"
    This reverts commit 71f8ca9e45ab94fd8e39e7a3b31f6cacbddf7850.

--- formatsp.c
+++ formatsp.c
@@ -18,10 +18,6 @@ void formatsp(const char *format, va_list args, int *n_printed)
 	case 's':
 		handle_string(va_arg(args, char *), n_printed);
 		break;
-	case 'S':
-		handle_string(va_arg(args, char *), n_printed);
-		break;
-
 	case 'c':
 		handle_character(args, n_printed);
 		break;
